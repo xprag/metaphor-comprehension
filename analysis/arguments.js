20e77f0b688ce8a351cfc3abc8afb06285cecef2
Antonio Pierro <antonio.pierro@gmail.com>
2014-10-21 23:32:29 +0200

    added a strict order and a trick to make the right color

--- analysis/arguments.js
+++ analysis/arguments.js
@@ -6,9 +6,24 @@ $(function () {
 
     $.getJSON('./arguments.json', function (json) {
 
-        tw_types = ['distrattore', 'O', 'P', 'L', 'V'];
+        // this array allows to keep a strict order.
+        tw_types = [
+            // 'distrattore_distrattore',
+            'O_TPTC', 'O_TPFC', 'O_TPPC',
+            'P_TPTC', 'P_TPFC', 'P_TPPC',
+            'L_TPTC', 'L_TPFC', 'L_TPPC',
+            'V_TPTC', 'V_TPFC', 'V_TPPC'
+        ];
 
         $.each(tw_types, function(index, tw_type) {
+
+            // TODO - This check is made to display the right color; red for wrong and green for correct answers.
+            if(json[tw_type].data.length === 1) {
+                if(json[tw_type].data[0][0] === 'Correct') {
+                    json[tw_type].data[1] = json[tw_type].data[0];
+                    json[tw_type].data[0] = ['Wrong', 0];
+                }
+            }
 
             $('#container').append($('<span>').attr('id', tw_type));
 
@@ -17,13 +32,13 @@ $(function () {
                     plotBackgroundColor: null,
                     plotBorderWidth: 1,//null,
                     plotShadow: false,
-                    width: 310
+                    width: 400
                 },
                 title: {
                     text: tw_type
                 },
                 tooltip: {
-                    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                    pointFormat: '{series.name}: <b>{point.y}</b> - <b>{point.percentage:.1f}%</b>'
                 },
                 plotOptions: {
                     pie: {
