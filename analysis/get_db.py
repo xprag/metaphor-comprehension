524a37082abf153c3c6650f0de67b92ceb5b7b0e
Antonio Pierro <antonio.pierro@gmail.com>
2014-10-24 11:47:44 +0200

    Revert "Analysis response time"

--- analysis/get_db.py
+++ analysis/get_db.py
@@ -11,12 +11,11 @@ DBSession.bind = engine
 session = DBSession()
 conn = engine.connect()
 
-# Query to get the questions response divided per argument.
 s = text("""
-	SELECT tw_type, argument_type, response_to_question, COUNT(*)
-	FROM argument, person
-	where person.id = argument.person_id
-	group by tw_type, argument_type, response_to_question
+SELECT tw_type, argument_type, response_to_question, COUNT(*)
+FROM argument, person
+where person.id = argument.person_id
+group by tw_type, argument_type, response_to_question
 """)
 json_data = {}
 for r in conn.execute(s).fetchall():
@@ -31,22 +30,6 @@ for r in conn.execute(s).fetchall():
 	except:
 		json_data[tw_type] = {}
 		json_data[tw_type]["data"] = [[label, r[3]]]
-f = open('arguments.json', 'w')
-f.write(json.dumps(json_data))
 
-# Query to get the average response time divided per argument.
-s = text("""
-	SELECT tw_type, argument_type, avg(response_time) as response_time_avg, COUNT(*)
-	FROM argument, person
-	where person.id = argument.person_id
-	group by tw_type, argument_type
-""")
-json_data = {}
-for r in conn.execute(s).fetchall():
-	if r[0] == 'distrattore':
-		tw_type = 'Distrattore'
-	else:
-		tw_type = str(r[0] + '_' + r[1])
-	json_data[tw_type] = r[2]
-f = open('response-time.json', 'w')
+f = open('arguments.json', 'w')
 f.write(json.dumps(json_data))
