b68accfcd4a80e5472b3e4c44f409007a54fb9d7
Antonio Pierro <antonio.pierro@gmail.com>
2014-11-13 09:25:42 +0100

    Added requirejs and created the js modules and templates

--- analysis/js/response-time.js
+++ analysis/js/response-time.js
@@ -1,66 +1,74 @@
-(function ($) {
+(function (define) {
     'use strict';
 
-    var data,
-        categories,
-        highchartConfig;
+    define([
+        'jquery',
+        'text!../templates/response-time-plot'
+    ], function ($,  responseTimePlotTemplate) {
 
-    data = [];
-    categories = [
-        'O_TPPC',
-        'P_TPPC',
-        'L_TPPC',
-        'V_TPPC'
-    ];
-    highchartConfig = {
-        chart: {
-            type: 'column',
-            width: 1200
-        },
-        title: {
-            text: 'Average Response Time'
-        },
-        subtitle: {
-            text: 'Source: metaphor-comprehension experiment'
-        },
-        xAxis: {
-            categories: categories
-        },
-        yAxis: {
-            min: 0,
+        var data,
+            categories,
+            highchartConfig;
+
+        data = [];
+        categories = [
+            'O_TPPC',
+            'P_TPPC',
+            'L_TPPC',
+            'V_TPPC'
+        ];
+        highchartConfig = {
+            chart: {
+                type: 'column',
+                width: 1200
+            },
             title: {
-                text: 'Average time (seconds)'
-            }
-        },
-        tooltip: {
-            headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-            pointFormat: '<tr><td>{series.name}: </td>' +
-                '<td style="padding:0"><b>{point.y:.2f} seconds</b></td></tr>',
-            footerFormat: '</table>',
-            shared: true,
-            useHTML: true
-        },
-        plotOptions: {
-            column: {
-                pointPadding: 0.2,
-                borderWidth: 0
-            }
-        },
-        series: [{
-            name: 'Response time',
-            color: '#aaff99',
-            data: data
-        }]
-    };
-    // Shorthand for $( document ).ready()
-    $(function () {
+                text: 'Average Response Time'
+            },
+            subtitle: {
+                text: 'Source: metaphor-comprehension experiment'
+            },
+            xAxis: {
+                categories: categories
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Average time (seconds)'
+                }
+            },
+            tooltip: {
+                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                pointFormat: '<tr><td>{series.name}: </td>' +
+                    '<td style="padding:0"><b>{point.y:.2f} seconds</b></td></tr>',
+                footerFormat: '</table>',
+                shared: true,
+                useHTML: true
+            },
+            plotOptions: {
+                column: {
+                    pointPadding: 0.2,
+                    borderWidth: 0
+                }
+            },
+            series: [{
+                name: 'Response time',
+                color: '#aaff99',
+                data: data
+            }]
+        };
 
-        $.getJSON('./json/response-time.json', function (json) {
-            categories.map(function (value) {
-                data.push(json[value]);
+        // Shorthand for $( document ).ready()
+        $(function () {
+            // It loads the template
+            $('.tab-content').append(responseTimePlotTemplate);
+            $.getJSON('./json/response-time.json', function (json) {
+                categories.map(function (value) {
+                    data.push(json[value]);
+                });
+                // It does not load the plot.
+                $('#reaction-time').highcharts(highchartConfig);
             });
-            $('#reaction-time').highcharts(highchartConfig);
         });
     });
-
-}(this.jQuery));
+}(this.define));
