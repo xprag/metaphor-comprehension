0657236b4235f86be36a9f14c10c5aa9f6df3cfd
Antonio Pierro <antonio.pierro@gmail.com>
2014-12-03 10:02:54 +0100

    Sorted columns into t-test table

---
+++ analysis/js/factories/tw-type-list.js
@@ -0,0 +1,13 @@
+(function (define) {
+    'use strict';
+
+    define([], function () {
+        // this array allows to keep a strict order when displaying the t-test table.
+        return [
+            'O_TPTC', 'O_TPFC', 'O_TPPC',
+            'P_TPTC', 'P_TPFC', 'P_TPPC',
+            'L_TPTC', 'L_TPFC', 'L_TPPC',
+            'V_TPTC', 'V_TPFC', 'V_TPPC'
+        ];
+    });
+}(this.define));
