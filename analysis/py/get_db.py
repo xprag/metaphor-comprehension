5cda3762e1f1bf5f903222ba43d80ae865fd8c1f
apierro <antonio.pierro@gmail.com>
2018-07-16 09:17:06 +0200

    Added getAnswersAnova

--- analysis/py/get_db.py
+++ analysis/py/get_db.py
@@ -10,6 +10,12 @@ from numpy import around, array, float, mean, std, ndarray
 import json
 import sys
 import os
+import csv
+import pandas as pd
+from statsmodels.formula.api import ols
+from statsmodels.stats.anova import anova_lm
+from statsmodels.graphics.factorplots import interaction_plot
+from scipy import stats
 
 db_file_name = 'arguments.db'
 if os.path.isfile(db_file_name):
@@ -164,31 +170,59 @@ def getResponseTimeTTest():
     return result
 
 # TODO:  avoid redundancy between getResponseTimeTTest and getAnswersTTest
+
 def getResponseTimeAnova():
-    import csv
-    import pandas as pd
-    from statsmodels.formula.api import ols
-    from statsmodels.stats.anova import anova_lm
-    from statsmodels.graphics.factorplots import interaction_plot
-    from scipy import stats
     s = text("""
-        SELECT tw_type, argument_type, response_time
+        SELECT person.id, avg(response_time), argument_type, tw_type
         FROM argument, person
         WHERE
         person.id = argument.person_id and
         argument_block <> 'P' and person.valid = 1 and
         tw_type <> 'distrattore'
+	    group by person.id, argument_type, tw_type
+        order by   person.id, argument_type, tw_type;
     """)
     file_name = 'responseTime.csv'
-    with open(file_name, 'wr') as csvfile:
+
+    with open(file_name, 'w') as csvfile:
         spamwriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
         spamwriter.writerow(['responseTime', 'argument', 'term'])
         for r in conn.execute(s).fetchall():
-            spamwriter.writerow([ r[2], r[1], r[0] ])
+            spamwriter.writerow([ r[1], r[2], r[3] ])
 
+    with open(file_name, 'r') as csvfile:
         data = pd.read_csv(file_name)
         formula = 'responseTime ~ C(argument) + C(term) + C(argument):C(term)'
+        model = ols(formula, data).fit()
+        print model
+        aov_table = anova_lm(model, typ=2)
+        print aov_table
+
+def getAnswersAnova():
+    s = text("""
+        SELECT
+        person.id, avg(response_to_question), argument_type, tw_type  as responses
+        FROM argument, person
+        WHERE
+        person.id = argument.person_id and
+        argument_block <> 'P' and person.valid = 1 and
+        tw_type <> 'distrattore'
+        group by person.id, argument_type, tw_type
+        order by   person.id, argument_type, tw_type;
+    """)
+    file_name = 'answers.csv'
+    with open(file_name, 'w') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(['answers', 'argument', 'term'])
+        for r in conn.execute(s).fetchall():
+            spamwriter.writerow([ r[1], r[2], r[3] ])
+
+        print "#######", file_name
+    with open(file_name, 'r') as csvfile:
+        data = pd.read_csv(file_name)
+        formula = 'answers ~ C(argument) + C(term) + C(argument):C(term)'
         model = ols(formula, data).fit()
         print model
         aov_table = anova_lm(model, typ=2)
@@ -239,3 +273,4 @@ write_json_file('t-test.json', json_data)
 getParticipantsGroupedByGender()
 getParticipantsGroupedByAge()
 getResponseTimeAnova()
+getAnswersAnova()
