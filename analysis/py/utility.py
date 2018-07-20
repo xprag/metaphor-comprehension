e0df70c096cc6cf3f0fa3055574286c86b0d3052
apierro <antonio.pierro@gmail.com>
2018-07-20 17:40:59 +0200

    added new comparisons

--- analysis/py/utility.py
+++ analysis/py/utility.py
@@ -3,7 +3,44 @@ def get_comparisons():
     # V -> NM
     # O -> H
     return [
-        ['CM-TPTC', 'H-TPFC'],
-        ['CM-TPFC', 'H-TPFC'],
-        ['CM-TPPC', 'H-TPFC']
+        ['H-TPTC', 'P-TPTC'],
+        ['H-TPFC', 'P-TPFC'],
+        ['H-TPPC', 'P-TPPC'],
+        ['H-TPTC', 'CM-TPTC'],
+        ['H-TPFC', 'CM-TPFC'],
+        ['H-TPPC', 'CM-TPPC'],
+        ['H-TPTC', 'NM-TPTC'],
+        ['H-TPFC', 'NM-TPFC'],
+        ['H-TPPC', 'NM-TPPC'],
+        ['P-TPTC', 'CM-TPTC'],
+        ['P-TPFC', 'CM-TPFC'],
+        ['P-TPPC', 'CM-TPPC'],
+        ['P-TPTC', 'NM-TPTC'],
+        ['P-TPFC', 'NM-TPFC'],
+        ['P-TPPC', 'NM-TPPC'],
+        ['CM-TPTC', 'NM-TPTC'],
+        ['CM-TPFC', 'NM-TPFC'],
+        ['CM-TPPC', 'NM-TPPC']
+    ]
+
+def get_comparisons_middleTerm():
+    return [
+        ['H', 'P'],
+        ['H', 'CM'],
+        ['H', 'NM'],
+        ['P', 'CM'],
+        ['P', 'NM'],
+        ['CM', 'NM']
+    ]
+
+def get_comparisons_argumentType():
+    return [
+        ['TPTC', 'TPFC'],
+        ['TPTC', 'TPPC'],
+        ['TPFC', 'TPPC']
+    ]
+
+def get_comparisons_letterali():
+    return [
+        ['H+PM', 'CM+NM']
     ]
