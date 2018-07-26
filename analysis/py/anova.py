bf8f5a54c63cd04ef6ded4ae4b4739015e5c07ce
apierro <antonio.pierro@gmail.com>
2018-07-26 16:53:31 +0200

    Imported utility

--- analysis/py/anova.py
+++ analysis/py/anova.py
@@ -4,6 +4,10 @@ import json, sys, os, csv
 import pandas as pd
 from statsmodels.formula.api import ols
 from statsmodels.stats.anova import anova_lm
+# my libs
+from utility import Utility
+from query import Query
+query = Query(0)
 
 db_file_name = 'arguments.db'
 if os.path.isfile(db_file_name):
@@ -12,37 +16,19 @@ else:
     raise Exception('The following file %s does not exist: ' % db_file_name)
 
 conn = engine.connect()
+utility = Utility(conn)
 
 def getAnswersAnova():
-    s = text("""
-        SELECT
-        person.id, avg(response_to_question), argument_type, tw_type  as responses
-        FROM argument, person
-        WHERE
-        person.id = argument.person_id and
-        argument_block <> 'P' and person.valid = 1 and
-        tw_type <> 'distrattore'
-        group by person.id, argument_type, tw_type
-        order by   person.id, argument_type, tw_type;
-    """)
-    file_name = 'answers.csv'
-    with open(file_name, 'w') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=',',
-                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        spamwriter.writerow(['answers', 'argument', 'term'])
-        for r in conn.execute(s).fetchall():
-            spamwriter.writerow([ r[1], r[2], r[3] ])
-
-
-    with open(file_name, 'r') as csvfile:
-        data = pd.read_csv(file_name)
-        formula = 'answers ~ C(argument) + C(term) + C(argument):C(term)'
-        model = ols(formula, data).fit()
-        aov_table = anova_lm(model, typ=2)
+    utility.write_file(query.get_accurancy_argumentType_middleTerm())
     print "\n\n####### getAnswersAnova #######"
-    print aov_table
+    print anova_lm(utility.get_model(), typ=2)
 
 def getResponseTimeAnova():
+    print query.get_responseTime_argumentType_middleTerm()
+    utility.write_file(query.get_responseTime_argumentType_middleTerm())
+    print anova_lm(utility.get_model(), typ=2)
+
+def getResponseTimeAnova_old():
     s = text("""
         SELECT person.id, avg(response_time), argument_type, tw_type
         FROM argument, person
